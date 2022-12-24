b97ee6b8b309cf5c122dab16901f82b8f1e845b8
Розум Сергей <sergej_r@list.ru>
2022-12-24 19:58:39 +0500

    Update 14.2

---
+++ 14.2/module.py
@@ -0,0 +1,20 @@
+import hvac
+import sys
+
+# Kubernetes (from k8s pod)
+client = hvac.Client(
+    url='http://vault:8200'
+)
+
+client.auth.approle.login(
+    role_id=sys.argv[1]
+)
+
+client.is_authenticated()
+
+# Читаем секрет
+secret = client.secrets.kv.v2.read_secret_version(
+    path='module',
+)
+
+print(secret['data']['data'])
